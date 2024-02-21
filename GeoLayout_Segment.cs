65590ccdc6e8e2f2e56b895c885a3f7e399b43e2
ThatCowGuy <cray40@gmx.de>
2024-02-21 01:10:02 +0100

    stubbed out geolayout seg

---
+++ GeoLayout_Segment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binjo
+{
+    public class GeoLayout_Command
+    {
+        public int cmd_id;
+
+    }
+
+    public class GeoLayout_Segment
+    {
+        public bool valid = false;
+
+        // this segment does not have a header...
+
+        // locators
+        public uint file_offset;
+
+        public void populate(byte[] file_data, int file_offset)
+        {
+            if (file_offset == 0)
+            {
+                System.Console.WriteLine("No Texture Segment");
+                this.valid = false;
+                return;
+            }
+            this.valid = true;
+            this.file_offset = (uint) file_offset;
+        }
+        public List<string[]> get_content()
+        {
+            List<string[]> content = new List<string[]>();
+
+            //===============================
+            content.Add(new string[] {
+                "File Offset",
+                File_Handler.uint_to_string(this.file_offset, 0xFFFFFFFF),
+                ""
+            });
+
+            return content;
+        }
+    }
+}
